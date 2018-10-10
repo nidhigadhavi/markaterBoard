0183422f24902022805a8505396384bd2e7d74db
nidhigadhavi <gadhavi019@gmail.com>
2018-10-10 00:15:47 -0400

    all rot files

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/app/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: "url-loader",
+        query: {
+          limit: 10000,
+          name: "images/[name].[hash:7].[ext]"
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: "url-loader",
+        query: {
+          limit: 10000,
+          name: "fonts/[name].[hash:7].[ext]"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+  })
+  ],
+  // devServer: {
+  //   contentBase: './dist',
+  //   hot: true
+  // }
+};
